1d7da31cdaaa820726bf1990f1aa1d4953689f17
davidruc <davidarueda18@gmail.com>
2023-08-04 07:14:07 -0500

    feat: :closed_lock_with_key: Configuration the validation in the body

--- middleware/limit.js
+++ middleware/limit.js
@@ -1,4 +1,4 @@
-import { rateLimit } from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 
 export let configGET = ()=>{
     return rateLimit({
@@ -6,6 +6,22 @@ export let configGET = ()=>{
         max: 5,
         standardHeaders: true,
         legacyHeaders:false,
-        message: {status: 429, message: "ya se te acabaron las solicitudes"}
-    })
+        skip: (req,res)=>{
+            if (req.headers["content-length"]>240) {
+                res.status(413).send({
+                    status: 413,
+                    message: "El tamaño es demasiado grande"
+                });
+                return true;
+            }
+        },
+        message: (req, res)=>{
+            res.status(429).send({
+                status: 429, 
+                message: "ya se te acabaron las solicitudes"
+            });
+        }
+})
 }
+
+//como evitar que se sobrescriban las colecciones cuando  sobrepasa el límite máximo
--- routes/automovil.js
+++ routes/automovil.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { configGET } from "../middleware/limit.js";
-import bodyParser from "body-parser";
 
 const appAutomovil = Router();
 
-appAutomovil.get("/", configGET(), bodyParser.json({limit: '151B'
-}) ,(req, res)=>{
-    console.log(req.body);
+appAutomovil.get("/", configGET() ,async(req, res)=>{
     console.log(req.rateLimit);
+    if(!req.rateLimit) return;
     res.send("hola mundo");
 })
 
