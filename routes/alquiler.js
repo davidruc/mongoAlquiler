99c111eeb7b483e541a1d076f602e87dee806e55
davidruc <davidarueda18@gmail.com>
2023-08-16 12:53:05 -0500

    feat: :zap: add the querys for the collection alquileres

--- routes/alquiler.js
+++ routes/alquiler.js
@@ -7,13 +7,130 @@ const appAlquiler = Router();
 let db = await conexion();
 let alquiler = db.collection("alquiler");
 
-appAlquiler.get("/:id?", configGET(),appMiddlewareAlquilerVerify,appDTOData,appDTOParam, async(req, res)=>{
+const getAlquileresById = (id)=>{
+    return new Promise(async(resolve)=>{
+        let result = await alquiler.aggregate([
+            { $match: { "ID_Alquiler": parseInt(id)}},
+            {
+                $project: {
+                    "_id": 0,
+                    "id_alquiler": "$ID_Alquiler",
+                    "inicio_alquiler": "$Fecha_Inicio",
+                    "fin_alquiler": "$Fecha_Fin",
+                    "costo_final": "$Costo_Total",
+                    "estado_alquiler": "$Estado",
+                }
+            }
+        ]).toArray();
+        resolve(result);
+    })
+};
+//A esta consulta hace falta meterle los datos del cliente
+const getAlquileresByTotalCliente = (TotalCliente)=>{
+    return new Promise(async(resolve)=>{
+        let result = await alquiler.aggregate([
+            {
+                $project: {
+                  "_id": 0,
+                  "id_alquier": "$ID_Alquiler",
+                  "id_cliente": "$ID_Cliente_id",
+                  "costo_final": "$Costo_Total"
+                }
+            },
+            {
+                $match: {
+                    "id_alquier": {$eq: parseInt(TotalCliente)}
+                }
+            }
+        ]).toArray();
+        resolve(result);
+    })
+};
+const getAllAlquileres = ()=>{
+    return new Promise(async(resolve)=>{
+        let result = await alquiler.aggregate([
+            {
+                $project: {
+                    "_id": 0,
+                    "id_alquiler": "$ID_Alquiler",
+                    "inicio_alquiler": "$Fecha_Inicio",
+                    "fin_alquiler": "$Fecha_Fin",
+                    "costo_final": "$Costo_Total",
+                    "estado_alquiler": "$Estado",
+                }
+            }
+        ]).toArray();
+        resolve(result);
+    })
+};
+appAlquiler.get("/", configGET(),appMiddlewareAlquilerVerify, appDTOParam, async(req, res)=>{
     if(!req.rateLimit) return;
-    let result = (!req.params.id)     
-    ? await alquiler.find({}).toArray()
-    : await alquiler.find({ "ID_alquiler": parseInt(req.params.id)}).toArray();
+    try{
+        const {id, TotalCliente} = req.query;
+        if(id){
+            const data = await getAlquileresById(id);
+            res.send(data);
+        } else if(TotalCliente){
+            const data = await getAlquileresByTotalCliente(TotalCliente);
+            res.send(data);
+        }
+        else {
+            const data = await getAllAlquileres();
+            res.send(data);
+        }
+    }
+    catch(err){
+        console.error("Ocurrió un error al procesar la solicitud", err.message);
+        res.sendStatus(500);
+    }
+});
+
+//Arreglar lo de las fechas no retorna nada
+appAlquiler.get("/dia", configGET(), appMiddlewareAlquilerVerify , async(req, res)=>{
+    if(!req.rateLimit) return;
+    let result = await alquiler.aggregate([
+        {
+            $match: {
+                "Fecha_Inicio": {$eq: "2024-02-15"}
+            }
+        }, {
+            $project: {
+                "_id": 0,
+                "id_alquiler": "$ID_Alquiler",
+                "inicio_alquiler": "$Fecha_Inicio",
+                "fin_alquiler": "$Fecha_Fin",
+                "costo_final": "$Costo_Total",
+                "estado_alquiler": "$Estado",
+            }
+        }
+    ]).toArray();
     res.send(result);
-})
+});
+appAlquiler.get("/plazos", configGET(), appMiddlewareAlquilerVerify , async(req, res)=>{
+    if(!req.rateLimit) return;
+    let result = await alquiler.find({
+        "Fecha_Inicio": {
+            $gte: "2023-08-11",
+            $lte: "2025-09-11"
+        }
+    }).toArray();
+    res.send(result);
+});
+//
+
+appAlquiler.get("/total", configGET(), appMiddlewareAlquilerVerify , async(req, res)=>{
+    if(!req.rateLimit) return;
+    let result = await alquiler.aggregate([
+        {$count: 'ID_Alquiler'},
+        {
+            $project: {
+                'Total de Alquileres' : '$ID_Alquiler'
+            }
+        }
+    ]).toArray();
+    res.send(result);
+});
+
 appAlquiler.post("/", configGET(), appMiddlewareAlquilerVerify, appDTOData, async(req, res)=>{
     if(!req.rateLimit) return;
     try{
