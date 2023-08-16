d8427059e379e5c3cc3dd44342b520fd31190541
davidruc <davidarueda18@gmail.com>
2023-08-16 06:27:25 -0500

    feat: :zap: New changes to genarete token for all collections

--- middleware/token.js
+++ middleware/token.js
@@ -4,24 +4,39 @@ import dotenv from 'dotenv';
 import {Router} from 'express';
 import { SignJWT, jwtVerify } from 'jose';
 import {Automovil} from "./DTO/Automovil.js";
+import {Alquiler} from "./DTO/Alquiler.js";
+import {Cliente} from "./DTO/Cliente.js";
+import {Empleado} from "./DTO/Empleado.js";
+import {Devoluciones} from "./DTO/registro_devolucion.js";
+import {Reservas} from "./DTO/Reserva.js";
+import {Entregas} from "./DTO/registro_entrega.js";
+import {SucursalAuto} from "./DTO/sucursal_automovil.js";
+import {Sucursal} from "./DTO/Sucursal.js";
+
 dotenv.config("../");
 const appToken = Router();
 const appVerify = Router();
 
 const createInstance = (className) => {
     const classMap = {
-      'autos': Automovil
+      'autos': Automovil,
+      'alquileres': Alquiler,
+      'datos_clientes': Cliente,
+      'datos_empleado': Empleado,
+      'devolucion': Devoluciones,
+      'reservacion': Reservas,
+      'entrega': Entregas,
+      'SucursalxAuto': SucursalAuto,
+      'Sucursales': Sucursal
     };
     const Class = classMap[className];
-    return (Class) ? plainToClass(Class, {}, { ignoreDecorators: true }) : undefined;
+    if(!Class) throw {status: 404, message: "Token solicitado no valido"}
+    return {atributos: plainToClass(Class, {}, { ignoreDecorators: true }), class: Class}
+    
 };
 appToken.use("/:collection", async(req,res)=>{
     try {
-        const collectionName = req.params.collection;
-        const inst = createInstance(collectionName);
-        if (!inst)
-        return res.status(404).send({ status: 404, message: "colección no encontrada" })
-
+        const inst = createInstance(req.params.collection).atributos;
         const encoder = new TextEncoder();
         const jwtconstructor = new SignJWT(Object.assign({}, classToPlain(inst)));
         const jwt = await jwtconstructor
@@ -32,7 +47,7 @@ appToken.use("/:collection", async(req,res)=>{
         req.data = jwt;
         res.status(201).send({status: 201, message: jwt});
     } catch (error) {
-        res.status(404).send({status: 404, message: "Token solicitado no valido"});
+        res.status(error.status).send(error);
     }
 })
 
@@ -53,5 +68,5 @@ appVerify.use("/", async(req,res,next)=>{
 })
 
 export {
-    appToken, appVerify
+    appToken, appVerify, createInstance
 };
