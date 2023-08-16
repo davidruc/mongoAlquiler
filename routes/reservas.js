01079b2c03a92b93a0908c132f02df867448723c
davidruc <davidarueda18@gmail.com>
2023-08-16 15:25:14 -0500

    feat: :sparkles: add the specific querys to the routes

--- routes/reservas.js
+++ routes/reservas.js
@@ -7,13 +7,97 @@ const appReservas = Router();
 let db = await conexion();
 let reservas = db.collection("reserva");
 
-appReservas.get("/:id?", configGET(),appMiddlewareReservaVerify, appDTOParam, async(req, res)=>{
+const getReservasById = (id)=>{
+    return new Promise(async(resolve)=>{
+        let result = await reservas.aggregate([
+            { $match: { "": parseInt(id)}},
+            {
+                $project: {
+                 "_id": 0,
+                 
+                }
+            }
+        ]).toArray();
+        resolve(result);
+    })
+};
+const getClientesReservasById = (idReserva)=>{
+    return new Promise(async(resolve)=>{
+        let result = await reservas.aggregate([{
+            $match: {
+                ID_Reserva: parseInt(idReserva)
+            }
+        },
+        {
+            $lookup: {
+                from: "cliente",
+                localField: "ID_Cliente_id",
+                foreignField: "ID_Cliente",
+                pipeline: [
+                    {
+                        $project: {
+                          "_id": 0,
+                          "id_cliente":"$ID_Cliente",
+                          "nombre_cliente":"$Nombre",
+                          "apellido_cliente":"$Apellido",
+                          "documento":"$DNI",
+                          "ubicacion":"$Direccion",
+                          "contacto":"$Telefono",
+                          "correo":"$Email"
+                        }
+                    }
+                ],
+                as: "info_cliente"
+            }
+        },
+        {
+            $project: {
+                "_id": 0,
+                "id_reserva": "$ID_Reserva",
+                "estado":"$Estado",
+                "info_cliente": "$info_cliente"
+            }
+        }
+    ]).toArray();
+        resolve(result);
+    })
+};
+const getAllReservas = ()=>{
+    return new Promise(async(resolve)=>{
+        let result = await reservas.aggregate([
+            {
+                $project: {
+                    "_id": 0,
+                    
+                }
+            }
+        ]).toArray();
+        resolve(result);
+    })
+};
+appReservas.get("/", configGET(),appMiddlewareReservaVerify, async(req, res)=>{
     if(!req.rateLimit) return;
-    let result = (!req.params.id)     
-    ? await reservas.find({}).toArray()
-    : await reservas.find({ "ID_Reserva": parseInt(req.params.id)}).toArray();
-    res.send(result);
-})
+    try{
+        const {id, idReserva} = req.query;
+        if(id){
+            const data = await getReservasById(id);
+            res.send(data);
+        } else if(idReserva){
+            const data = await getClientesReservasById(idReserva);
+            res.send(data);
+        }
+        else {
+            const data = await getAllReservas();
+            res.send(data);
+        }
+    }
+    catch(err){
+        console.error("Ocurrió un error al procesar la solicitud", err.message);
+        res.sendStatus(500);
+    }
+});
+
+
 appReservas.post("/", configGET(), appMiddlewareReservaVerify, appDTOData, async(req, res)=>{
     if(!req.rateLimit) return;
     try{
